dd09b9a09254f21dd14239ed20ca57736dd816d8
Retchizu <baltazarrichmond31@gmail.com>
2023-05-12 20:45:42 +0800

    Main method changes

--- Project/src/Number1.java
+++ Project/src/Number1.java
@@ -5,35 +5,74 @@ public class Number1{
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
 
-        int[] optionsNumbers = {1, 2, 3, 4};
-        String[] options = {"Leap || !Leap", "Height A > Height B == Height C", "Series", "Shine bright like a diamond"};
+        String[] options = {"Leap Year Checker", "Compare Height", "Series", "Generate a Diamond", "Exit"};
 
-        String name = stringPrompt("Enter your name: ");
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
         System.out.println("Hello, " + name + "!\n");
 
-        System.out.println("Tell me what you want to do, here are your options: ");
-        for (int i = 0; i < optionsNumbers.length; i++) {
-            System.out.println(optionsNumbers[i] + "." + options[i]);
-        }
-
-        while (true) {
-            try {
-                int userInput = intPrompt("Enter your choice: ");
-                if (userInput >= 1 && userInput <= 4) {
-                    System.out.println("Success");
-                    break;
-                } else {
-                    System.out.println("Your input is not within the choices. Please try again.");
+        int option = -1;
+        while(option != 5){
+            //choicesmenu
+            System.out.println("Tell me what you want to do, here are your options: ");
+            for(int i = 0, number = 1; i < options.length; i++){
+            System.out.println(number++ + ". " + options[i]);
+            }
+            System.out.println();
+            try{
+                System.out.print("Enter your choice: ");
+                option = input.nextInt();
+                switch(option){
+                    case 1:
+                        System.out.print("Enter a year: ");
+                        int year = input.nextInt();
+                        if(leapYearOrNot(year)) System.out.println("Year " + year + " is a leap year.\n");
+                        else System.out.println("Year " + year + " is not a leap year.\n");
+                        break;
+                    case 2:
+                        System.out.print("Enter height of person 1(cm): ");
+                        int a = input.nextInt();
+                        System.out.print("Enter height of person 2(cm): ");
+                        int b = input.nextInt();
+                        System.out.print("Enter height of person 3(cm): ");
+                        int c = input.nextInt();
+                        System.out.println(compareHeights(a, b , c));
+                        System.out.println();
+                        break;
+                    case 3:
+                        System.out.print("Enter the value of n: ");
+                        int n = input.nextInt();
+                        fibonacciSeries(n);
+                        System.out.println();
+                        break;
+                    case 4:
+                        System.out.print("Enter the size of diamond(starting from top to middle): ");
+                        int size = input.nextInt();
+                        createDiamond(size);
+                        System.out.println();
+                        break;
+                    case 5:
+                        System.out.println("Program Exited.\n");
+                        break;
+                    default:
+                        System.out.println("Invalid choice, choose from 1 to 4!\n");
+                        break;
                 }
-            } catch (NumberFormatException exception) {
-                System.out.println("Your input is invalid, please try again.");
-            } catch (InputMismatchException exception) {
-                System.out.println("Only numbers are allowed, please try again.");
+            }catch(NumberFormatException e){
+                //user input a wrong number format
+                System.out.println("Only whole numbers are allowed, please try again.");
+                input.nextLine();
+            }catch(InputMismatchException e){
+                //wrong input from scanner example: int but input was string
+                System.out.println("Your input is invalid, please enter a correct format");
                 input.nextLine();
             }
         }
+        input.close();  
     }
 
+    //Methods used is switch case
+    //Leap year checker method
     public static boolean leapYearOrNot(int year) {
         if (year % 4 != 0) {
             return false;
@@ -41,7 +80,8 @@ public class Number1{
             return true;
         } else return year % 400 == 0;
     }
-
+    
+    //Compare height method
     public static String compareHeights(int a, int b, int c) {
         if (a == b && b == c) {
             return "All persons have equal height.";
@@ -72,6 +112,8 @@ public class Number1{
         }
     }
 
+    //fibonacci series method
+    //find a way to do recursively?
     public static void fibonacciSeries(int limit) {
         int firstNumber = 1;
         int secondNumber = 1;
@@ -86,6 +128,7 @@ public class Number1{
         }
     }
 
+    //diamond generator method
     public static void createDiamond(int rowsFromMiddle) {
         int numberOfColumns = rowsFromMiddle * 2 - 1;
         for (int i = 1; i <= rowsFromMiddle; i++) {
@@ -106,17 +149,5 @@ public class Number1{
             }
             System.out.println();
         }
-    }
-
-    public static String stringPrompt(String prompt)
-    {
-        System.out.print(prompt);
-        return input.next();
-    }
-
-    public static int intPrompt(String prompt)
-    {
-        System.out.print(prompt);
-        return input.nextInt();
     }
 }
