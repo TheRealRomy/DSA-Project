f726e247e90856313cad04b10bcffeb25a17667e
Retchizu <baltazarrichmond31@gmail.com>
2023-05-15 02:37:52 +0800

    added method to evaluate tree

--- Project/src/Number2.java
+++ Project/src/Number2.java
@@ -1,23 +1,22 @@
 import java.util.*;
 
-
-
 public class Number2 {
 
     static class Node {
         char data;
         Node left, right;
         int x;
-    
+
         Node(char data) {
             this.data = data;
             left = null;
             right = null;
             x = 0;
         }
+
     }
 
-    //postfix method for expression
+    // postfix method for expression
     public static String infixToPostfix(String expression) {
         StringBuilder postfix = new StringBuilder();
         Stack<Character> stack = new Stack<>();
@@ -51,13 +50,13 @@ public class Number2 {
         return postfix.toString().trim();
     }
 
-    //prefix method for expression
+    // prefix method for expression
     public static String infixToPrefix(String infix) {
         String reversed = reverseString(infix);
         StringBuilder result = new StringBuilder();
         Stack<Character> stack = new Stack<>();
         for (char c : reversed.toCharArray()) {
-            if(c == ' '){
+            if (c == ' ') {
                 continue;
             } else if (Character.isLetterOrDigit(c)) {
                 result.append(c + " ");
@@ -80,8 +79,8 @@ public class Number2 {
         }
         return result.reverse().toString().trim();
     }
-    
-    //reverses the string(for prefix)
+
+    // reverses the string(for prefix)
     public static String reverseString(String str) {
         if (str == null || str.length() <= 1) {
             return str;
@@ -89,7 +88,7 @@ public class Number2 {
         return reverseString(str.substring(1)) + str.charAt(0);
     }
 
-    //for prefix and postfix stack algorithm
+    // for prefix and postfix stack algorithm
     private static int precedenceOrder(char operator) {
         switch (operator) {
             case '+':
@@ -105,18 +104,19 @@ public class Number2 {
         }
     }
 
-    //checks if the character is an operator
+    // checks if the character is an operator
     private static boolean isOperator(char character) {
         return character == '+' || character == '-' || character == '*' || character == '/' || character == '^';
     }
 
-    //method that generates an expression tree from postfix expression
+    // method that generates an expression tree from postfix expression
     public static Node constructExpressionTree(String postfix) {
         char[] characters = postfix.toCharArray();
         Stack<Node> stack = new Stack<Node>();
 
         for (char c : characters) {
-            if(c == ' ') continue;
+            if (c == ' ')
+                continue;
             else if (isOperator(c)) {
                 Node rightNode = stack.pop();
                 Node leftNode = stack.pop();
@@ -134,82 +134,129 @@ public class Number2 {
         return stack.pop();
     }
 
-    //display inorder tree
-    public static void displayInorderTree(Node root) {
+    // display inorder tree
+    public static void displayInorderTreeStructure(Node root) {
         if (root == null)
             return;
 
-        displayInorderTree(root.left);
+        displayInorderTreeStructure(root.left);
         System.out.print(root.data + " ");
-        displayInorderTree(root.right);
+        displayInorderTreeStructure(root.right);
     }
 
-    //checks if the node is leaf
+    // checks if the node is leaf
     public static boolean isLeaf(Node root) {
         return root.left == null && root.right == null;
     }
 
-    //method to update variable
-    public static void updateVariable(Node root, HashMap<Character, Integer> map, int userInput){
-        if(root == null) return;
+    // method to update variable
+    public static void updateVariable(Node root, HashMap<Character, Integer> map, int userInput) {
+        if (root == null)
+            return;
 
-        if(isLeaf(root)){
+        if (isLeaf(root)) {
             map.put(root.data, userInput);
-            root.x = map.getOrDefault(root.data, userInput); //A , 23 --> map.get('A') = 23
+            root.x = map.getOrDefault(root.data, userInput); // A , 23 --> map.get('A') = 23
         }
     }
 
-    public static String displayUpdatePrefixAndPostfix(String expression, HashMap<Character, Integer> map){
-        if(expression == null || expression.isEmpty()) return null;
+    //display postfix and prefix of updated expression tree
+    public static String displayUpdatePrefixAndPostfix(String expression, HashMap<Character, Integer> map) {
+        if (expression == null || expression.isEmpty())
+            return null;
 
         StringBuilder sb = new StringBuilder();
 
-        for(char c : expression.toCharArray()){
-            if(c == ' '){
+        for (char c : expression.toCharArray()) {
+            if (c == ' ') {
                 continue;
-            }
-            else if(Character.isLetterOrDigit(c)){
-                if(map.get(c) == null){
+            } else if (Character.isLetterOrDigit(c)) {
+                if (map.get(c) == null) {
                     sb.append("0 ");
-                }else{
+                } else {
                     sb.append(map.get(c) + " ");
                 }
-            }else if(isOperator(c)){
+            } else if (isOperator(c)) {
                 sb.append(c + " ");
             }
         }
         return sb.toString();
     }
+
+    //evaluate tree(user optional)
+    public static int evaluateTree(String postfix, HashMap<Character, Integer> map) {
+        Stack<Integer> nums = new Stack<>();
+        int result = 0;
+        for (char c : postfix.toCharArray()) {
+            try{
+                if (c == ' ') {
+                    continue;
+                } else if (Character.isLetterOrDigit(c)) {
+                    nums.push(map.get(c));
+                } else if (isOperator(c)) {
+    
+                    while (!nums.isEmpty()) {
+                        switch (c) {
+                            case '+':
+                                result += nums.pop();
+                                break;
+                            case '-':
+                                result -= nums.pop();
+                                break;
+                            case '*':
+                                result *= nums.pop();
+                                break;
+                            case '/':
+                                result /= nums.pop();
+                                break;
+                        }
+                    }
+    
+                }
+            }catch(Exception e){
+                System.out.println("Can't Perform Arithmetic Operation, Try changing the leaf order");
+            }
+            
+        }
+
+        return result;
+    }
+
+    //display the updated expression tree(infix structure)
+    public static String displayUpdatedExpression(String expression, HashMap<Character, Integer> map){
+        StringBuilder sb = new StringBuilder();
+        for(char c : expression.toCharArray()){
+            if(c == ' '){
+                continue;
+            } else if (Character.isLetterOrDigit(c)) {
+                if (map.get(c) == null) {
+                    sb.append("0 ");
+                } else {
+                    sb.append(map.get(c) + " ");
+                }
+            } else if (isOperator(c)) {
+                sb.append(c + " ");
+            }
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         HashMap<Character, Integer> map = new HashMap<>();
-        String expresion = "(A + B - C)";
-        String postfixOfExpression = infixToPostfix(expresion);
-        Node r = constructExpressionTree(postfixOfExpression);
+        String expression = "(A + B - C)";
+        String postfix = infixToPostfix(expression);
+        Node root = constructExpressionTree(postfix);
         System.out.print("Expression Tree: ");
-        displayInorderTree(r);
+        displayInorderTreeStructure(root);
         System.out.println();
-        System.out.println("root: "+ r.data); // -
-        System.out.println("root.left: " + r.left.data); // + 
-        System.out.println("root.left.left:  " + r.left.left.data); // A
-        System.out.println("root.left.right: "+ r.left.right.data); // B
-        System.out.println("root.right: " + r.right.data); // C
-
-        System.out.println("Before update: "+ r.left.left.x);
-        updateVariable(r.left.left, map, 23); 
-        System.out.println("After update: " + + r.left.left.x);
-
-        System.out.println("prefix : " + infixToPrefix(expresion));
-        System.out.println("postfix : " +infixToPostfix(expresion));
-
+        updateVariable(root.left.left, map, 23); //A
+        updateVariable(root.left.right, map, 12);//B
+        updateVariable(root.right, map, 69); //C
         
-        String prefix = infixToPrefix(expresion);
-        System.out.println();//new line in console
-        updateVariable(r.left.right, map, 12);
-        updateVariable(r.right, map, 69);
-
-        System.out.println(displayUpdatePrefixAndPostfix(prefix, map));
-        System.out.println(displayUpdatePrefixAndPostfix(postfixOfExpression, map));
-
+        System.out.print(displayUpdatedExpression(expression, map));
+        System.out.print(" = ");
+        System.out.println(evaluateTree(postfix, map));
         
+
     }
 }
